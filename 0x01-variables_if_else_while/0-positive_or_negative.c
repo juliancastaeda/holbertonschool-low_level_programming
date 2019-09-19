f792ea0a02095fbdb12d85c96ebdefbe48ab5cae
juliancastaeda <1277@holbertonschool.com>
2019-09-19 22:29:13 +0000

    w

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	}
 
 	return (0);
