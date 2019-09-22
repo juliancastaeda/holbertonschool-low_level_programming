f124a575164711576e7dc7a0e1fddeede6f119f7
juliancastaeda <1277@holbertonschool.com>
2019-09-22 23:53:13 +0000

    load point 3 alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - star data
+ *
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	char n;
+	char j;
+
+	for (n = 'a'; n <= 'z'; n++);
+	putchar(n);
+	for (j = 'A'; j <= 'Z'; j++);
+	putchar(j);
+	putchar('\n');
+	return (0);
+}
