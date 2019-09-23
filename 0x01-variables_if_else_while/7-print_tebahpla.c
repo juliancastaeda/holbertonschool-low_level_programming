f85e19b566a82dbb20ed6d6d75c347d5a2ff3c67
juliancastaeda <1277@holbertonschool.com>
2019-09-23 02:45:08 +0000

    load point 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - star data
+ *
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+
+	putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
