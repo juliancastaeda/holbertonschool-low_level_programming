5c6003da214992863b8073c211144a0996448704
juliancastaeda <1277@holbertonschool.com>
2019-09-23 01:49:14 +0000

    w

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - star data
+ *
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for ( n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
