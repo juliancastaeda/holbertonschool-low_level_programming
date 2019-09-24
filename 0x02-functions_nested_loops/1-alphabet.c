0afb5569bff1c3df78040ba2349caf92a865d57b
juliancastaeda <1277@holbertonschool.com>
2019-09-24 21:06:00 +0000

    load commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
  *
  * Return: Alaways 0 (Success)
  */
-
 void print_alphabet(void)
 
 {
 	int n;
 
-	for (n = 'a'; n <= 'z'; n++);
-	_putchar('n');
-	_putchar('\n');
-
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar('n');
+		_putchar('\n');
+	}
 }
