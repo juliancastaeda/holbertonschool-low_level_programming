452f97d98c2748c5f0e76056c0bd00f37e28cfa1
juliancastaeda <1277@holbertonschool.com>
2019-09-25 18:48:05 +0000

    load commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * _islower - print 1 or 0.
- * @c:evaluate if it is capital
+ * _isalpha - print 1 or 0.
+ * @c:evaluate if it is uppercase and print 1 or 0
  *
  * Return: Alaways 0 (Success)
  */
@@ -10,12 +10,11 @@
 int _isalpha(int c)
 {
 	if (c >= 65 || c >= 122)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
-
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
