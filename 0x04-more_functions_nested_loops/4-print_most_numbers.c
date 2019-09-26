24f813fdd00abaae78e3b18433051079a6a64417
juliancastaeda <1277@holbertonschool.com>
2019-09-26 19:37:21 +0000

    load commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - print numbers.
- * @c:evaluate if it is capital
+ * @ :
  *
  * Return: Alaways 0 (Success)
  */
@@ -11,12 +11,10 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n == '0'; n <= '9'; n++)
 	if (n != '2' && n != '4')
 	{
 		_putchar(n);
 	}
 	_putchar('\n');
-
-
 }
