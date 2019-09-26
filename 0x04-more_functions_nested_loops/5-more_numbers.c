5cec515da4abc1d6ce8e106dcef18bd816e696f2
juliancastaeda <1277@holbertonschool.com>
2019-09-26 20:18:18 +0000

    load commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_more_numbers - print numbers.
+ * @ :
+ *
+ * Return: Alaways 0 (Success)
+ */
+
+void print_more_numbers(void)
+{
+	int n, m;
+
+	while (m <= 9)
+	for (n = '0' n <= "14"; n++)
+
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	++m;
+}
