3f150bc94a545ac7afb7a76b7bb72bfe5262e72a
juliancastaeda <1277@holbertonschool.com>
2019-09-26 18:56:28 +0000

    load comit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _islower - print 1 or 0.
+ * @c:evaluate if it is capital
+ *
+ * Return: Alaways 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,3 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
