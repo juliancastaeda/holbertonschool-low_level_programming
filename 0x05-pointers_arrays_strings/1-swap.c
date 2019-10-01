1f49e835addccc6f2b16182a5cc86080a2381f31
juliancastaeda <1277@holbertonschool.com>
2019-10-01 15:13:56 +0000

    load commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * swap_int change numerical values between a and b.
- * @a: number exchange with b
- * @b: exchange of numbers with a
+ * swap_int - change numerical values between a and b.
+ * @a: number exchange with b.
+ * @b: exchange of numbers with a.
  *
  */
 
