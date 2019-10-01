93af3f7d6cd4f47f1ea1b85ee0bc010991bde79d
juliancastaeda <1277@holbertonschool.com>
2019-10-01 21:23:41 +0000

    load commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - change numerical values between a and b.
+ * @a: number exchange with b.
+ * @b: exchange of numbers with a.
+ * Return: Alaways 0 (Success)
+ */
+
+void _puts(char *str)
+
+{
+	char c[53];
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
