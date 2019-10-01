b207ae69e36682e498a5daca731f05e3e61892f8
juliancastaeda <1277@holbertonschool.com>
2019-10-01 15:22:54 +0000

    load commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,7 @@
  * swap_int - change numerical values between a and b.
  * @a: number exchange with b.
  * @b: exchange of numbers with a.
- *
+ * Return: Alaways 0 (Success)
  */
 
 void swap_int(int *a, int *b)
