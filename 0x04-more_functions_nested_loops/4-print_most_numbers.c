4aa23cdc30f73a5c1e392b965d2ca6fe6c2197a8
juliancastaeda <1277@holbertonschool.com>
2019-10-03 03:58:10 +0000

    load commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,12 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
-	if (n != '2' && n != '4')
+	for (n = 48; n <= 57; n++)
 	{
+		if (n != 50 && n != 52)
+		{
 		_putchar(n);
+		}
 	}
 	_putchar('\n');
 }
