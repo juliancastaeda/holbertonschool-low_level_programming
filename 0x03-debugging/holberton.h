74285cad1979c7dfd01374274d49992cae2c690d
juliancastaeda <1277@holbertonschool.com>
2019-10-03 02:52:13 +0000

    load commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,9 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int _isupper(int c);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* HOLBERTON_H */
