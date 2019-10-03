b10add7befc7dca8fa4c1e0a9f35c665d0fd2f08
juliancastaeda <1277@holbertonschool.com>
2019-10-03 22:26:14 +0000

    load commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncpy - return string
+ * @dest: printer
+ * @src: printer
+ : @n :printer
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	for(a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	for(a = 0; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+	return (dest);
+}
