e673a98408566d0c79c0f79014ebb7d35aa4660e
juliancastaeda <1277@holbertonschool.com>
2019-10-06 05:27:30 +0000

    load commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: cero
+ */
+int main(void)
+{
+	int i = 49;
+	int j = 48;
+	int k = 50;
+	int a = 44;
+	int b = 32;
+	int c = 49;
+	int d = 50;
+	int e = 50;
+
+	for (; j <= 55; j++)
+	{
+		for (; i <= 56; i++)
+		{
+			for (; k <= 57; k++)
+			{
+				putchar(j);
+				putchar(i);
+				putchar(k);
+				if (((j == 55) && (i == 56)) && (k == 57))
+					break;
+				putchar(a);
+				putchar(b);
+			}
+			d++;
+			k = d;
+		}
+		e++;
+		k = e;
+		d = e;
+		c++;
+		i = c;
+	}
+	putchar('\n');
+	return (0);
+}
