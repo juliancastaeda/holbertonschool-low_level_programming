81fc09ce9425601ad216c7416b8cdc8d509884b6
juliancastaeda <1277@holbertonschool.com>
2019-10-06 03:57:56 +0000

    load commit

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,10 +8,18 @@
  */
 int main(void)
 {
-	char n;
-
-	for ( n = "0"; n <= "9"; n++)
-	putchar("n");
+	int n;
+	for (n = 0; n < 100; n++)
+	{
+	putchar((n / 10) + '0');
+	putchar((n % 10) + '0');
+	if (n < 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	else
 	putchar('\n');
+	}
 	return (0);
 }
