32f6ce914a3eff3593e9dcb3a207fb87cdf1d693
juliancastaeda <1277@holbertonschool.com>
2019-10-07 18:22:40 +0000

    load commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer
+ * @b: memory
+ * @n: memory
+ *
+ * Return: Char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x = 0;
+
+	for (x = 0; x <= n; i++)
+	{
+		s[i] = b;
+	}
+	return(s);
+}
