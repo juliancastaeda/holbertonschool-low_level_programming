ddc9ffd1efda619f01d25f8837965513721dd6f6
juliancastaeda <1277@holbertonschool.com>
2019-10-07 03:12:28 +0000

    load commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string.
+ * @p: first value to compare
+ *
+ * Return: Char
+ */
+char *leet(char *p)
+{
+	int i = 0, j;
+	char a[] = {"a4A4e3E3o0O0l1L1t7T7"};
+
+	while (p[i] != '\0')
+	{
+		for (j = 0; j <= 19; j++)
+		{
+			if (((p[i] >= 65 && p[i] <= 90) ||
+			     (p[i] >= 97 && p[i] <= 122)) &&
+			    (p[i] == a[j]))
+				p[i] = a[j + 1];
+		}
+		i++;
+	}
+	return (p);
+}
