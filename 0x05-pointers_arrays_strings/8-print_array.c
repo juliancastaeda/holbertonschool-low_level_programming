a0a1ec9e6d26878b0d883e7edeaf676174679eab
juliancastaeda <1277@holbertonschool.com>
2019-10-07 02:50:48 +0000

    load commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array.
+ *@a: first value to check
+ *@n: Second value to check
+ *
+ */
+void print_array(int *a, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n;  i++)
+	{
+		printf("%i", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
