73489a8559374b0f342191f5971b5095d4ff1b60
juliancastaeda <1277@holbertonschool.com>
2019-10-09 21:46:32 +0000

    load commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - print namber factor
+ * @n : recursion
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+	return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+		{
+			return (1);
+		}
+	else
+	{
+		return (-1);
+	}
+}
