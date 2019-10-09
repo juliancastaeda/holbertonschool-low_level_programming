6375308bc982341fc7aab3e76cbd58f8fd1730f0
juliancastaeda <1277@holbertonschool.com>
2019-10-09 00:05:49 +0000

    load commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strstr - Str
+ * @haystack: pointer
+ * @needle:pointer
+ *
+ * Return: char
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int n, m;
+	if (*needle == 0)
+		return (haystack);
+	for (n = 0; haystack[n] != '\0'; n++)
+	{
+		for (m = 0; needle[m] != '\0'; m++)
+		{
+			if (haystack[n + m] != needle[m])
+			{
+				break;
+			}
+		}
+		if (needle[m] == '\0')
+			return (haystack + n);
+	}
+	return (0);
+}
