6dbf953ab11218b8b1462921a4f6796d42dd0864
juliancastaeda <1277@holbertonschool.com>
2019-10-13 16:51:51 +0000

    load commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the new name
+ * @argc: arguments
+ * @argv: array the arguments
+ *
+ * Return: always (0)
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
