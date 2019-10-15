1385d858ccb70cd9e85e06f40a841bab86a6eaa8
juliancastaeda <1277@holbertonschool.com>
2019-10-15 19:50:20 +0000

    load commit

---
+++ 0x0B_malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array- create array and allocate memory
+ * @size:value of the memory use
+ * @c : variable
+ * Return: (0).
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *n;
+	unsigned int m = 0;
+	if (size != 0)
+	{
+	n = malloc(size * sizeof(c));
+	}
+	else
+	{
+	return (NULL);
+	}
+	if (n == 0)
+	{
+	return (NULL);
+	}
+	while (m < size)
+	{
+	n[m] = c;
+	m++;
+	}
+	return (n);
+}
---
+++ 0x0B_malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array- create array and allocate memory
+ * @size:value of the memory use
+ * @c : variable
+ * Return: (0).
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+
+
+       }
