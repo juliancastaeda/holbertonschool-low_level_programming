6aac90ae76ba9fafb0da4c0fb68c7d8a76ed9ac7
juliancastaeda <1277@holbertonschool.com>
2019-10-17 18:06:11 -0500

    load commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - concat  s1 s2.
+ * @min: array
+ * @max: bytes array
+ *
+ * Return: (0).
+ *
+ */
+ int *array_range(int min, int max)
+ {  int *a;
+ int b;
+
+    if (min > max)
+    {
+        return(NULL);
+    }
+    a = malloc(sizeof(int) * (max - min + 1));
+    if (a == NULL)
+    {
+        return(NULL);
+    }
+    for (b = 0; b < (max - min +1); b++)
+    {
+        a[b] = (min + b);
+    }
+    return (a);
+  }
