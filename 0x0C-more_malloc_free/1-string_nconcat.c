239b58f61012f98a537e07be826e1183d0243069
juliancastaeda <1277@holbertonschool.com>
2019-10-17 14:46:57 -0500

    load commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,5 +10,33 @@
  */
  char *string_nconcat(char *s1, char *s2, unsigned int n)
  {
-     
- }
+    char *p;
+	unsigned int t1 = 0, t2 = 0, i = 0;
+
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+
+	while (s1[t1] != '\0')
+	t1++;
+	while (s2[t2] != '\0')
+	t2++;
+	t2++;
+	p = malloc((t1 + t2) * (sizeof(char)));
+	if (p == 0)
+	return (NULL);
+	while (i < t1)
+	{
+	p[i] = s1[i];
+	i++;
+	}
+	i = 0;
+	while (i < t2)
+	{
+	p[i + t1] = s2[i];
+	i++;
+	}
+	p[t1 + t2] = '\0';
+	return (p);
+}    
