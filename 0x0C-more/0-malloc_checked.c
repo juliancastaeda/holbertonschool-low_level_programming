1b6980b7430d354fa87fac69954174c4fe4e8e03
juliancastaeda <1277@holbertonschool.com>
2019-10-17 18:18:52 +0000

    load commit

---
+++ 0x0C-more/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked- create
+ * @grid: free memory
+ * @height: array
+ * Return: (0).
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *a;
+
+	a = (malloc(b));
+
+	if (a == NULL)
+		{
+	exit (98);
+		}
+	return (a);
+       }
