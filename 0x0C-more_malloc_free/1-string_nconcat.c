8551ade3443d8ea2d470d41a79ae2fc6324398a9
juliancastaeda <1277@holbertonschool.com>
2019-10-17 14:24:12 -0500

    load commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked- array NULL
+ * @b:variable
+ *
+ * Return: (0).
+ *
+ */
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
+ {
+     
+ }
