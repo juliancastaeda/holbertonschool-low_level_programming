c7e83b8ba975f5f4750f509fa868f2661ddf4bae
juliancastaeda <1277@holbertonschool.com>
2019-10-21 21:22:13 -0500

    load commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,22 @@
 #define identifer
 
 /**
- * struct identifer_name - Descripción 
- * dog - strctured
- * @name: variable name
- * @age: variable age
- * @owner: vriable owner
- *
- *  Description: Dog variable 
- *
- */
- struct dog
- {
-     char *name;
-     float age;
-     char *owner;
- };
- typedef struct dog dog_t;
- void print_dog(struct dog *d);
- void init_dog(struct dog *d, char *name, float age, char *owner);
- #endif
+* struct dog - Descripción
+* dog - strctured
+* @name: variable name
+* @age: variable age
+* @owner: vriable owner
+*
+* Description: Dog variable
+*
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
