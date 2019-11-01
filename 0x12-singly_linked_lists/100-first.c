b71730f47c7e9bfc57324e882e45b6907465888e
juliancastaeda <1277@holbertonschool.com>
2019-10-31 23:28:47 -0500

    load first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  * tortoise - Hare_and_the_Tortoise
+ **/
+void tortoise (void) __attribute__ ((constructor));
+void tortoise (void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
