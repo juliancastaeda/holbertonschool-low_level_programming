a07aafc507ba6be1a80b27c7430793201d868324
juliancastaeda <1277@holbertonschool.com>
2019-11-05 09:33:17 -0500

    upload task 5 and lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint2 - free lists
+ * @head: singly listen
+ *
+ * Return:  free lists
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *b = *head;
+
+while (*head != NULL)
+{
+b = (*head)->next;
+free(b);
+*head = NULL;
+}
+}
