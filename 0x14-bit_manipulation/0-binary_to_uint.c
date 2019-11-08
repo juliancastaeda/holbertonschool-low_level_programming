fa40fa9ef7aebf7d4061e7a31568b3fb2552a67d
juliancastaeda <1277@holbertonschool.com>
2019-11-07 19:50:36 -0500

    load point 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - add node index
+ * @b: pointer a char
+ *
+ * Return: int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int c = 0;
+int n = 0, m = 0, a = 1;
+
+while (b == NULL)
+{
+return (0);
+}
+while (b[n] != '\0')
+{
+n++;
+}
+for (m = n - 1; m >= 0; m--)
+{
+if (b[m] != '0' || b[m] != '1')
+{
+return (0);
+}
+if (b[m] == '1')
+{
+c = c + a;
+}
+a = a * 2;
+}
+return (c);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+#endif
