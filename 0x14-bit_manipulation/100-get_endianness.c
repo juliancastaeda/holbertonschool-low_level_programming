478488194045d92a9c632e59a13582e9a403ff30
juliancastaeda <1277@holbertonschool.com>
2019-11-07 21:52:03 -0500

    upolad 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 
 /**
- * print_binary - convert decimal to binary
- * @n: variable
+ * get_endianness - endianness
+ *
  *
  * Return: (0)
  */
- int get_endianness(void)
- {
-     int16_t a = 1;
-     int8_t *p = (int8_t *) &a;
+int get_endianness(void)
+{
+int16_t a = 1;
+int8_t *p = (int8_t *) &a;
 
-     if (p[0] == 1)
-     {
-         return (1);
-     }
-     else
-     {
-         return (0);
-     }
- } 
+if (p[0] == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
