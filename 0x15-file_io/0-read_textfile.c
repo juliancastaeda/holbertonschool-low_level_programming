03e04206f957a6841bfe8a17939e41ea7a12c9a4
juliancastaeda <1277@holbertonschool.com>
2019-11-12 16:01:33 -0500

    load

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,14 +18,14 @@ return (0);
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-ptr = malloc(sizeof(letters));
+ptr = malloc(sizeof(ptr) * letters);
 if (ptr == NULL)
 return (0);
 a = read(fd, ptr, letters);
 state = write(STDOUT_FILENO, ptr, a);
 if (state == -1)
 {
-return (-1);
+return (0);
 }
 free(ptr);
 close(fd);
