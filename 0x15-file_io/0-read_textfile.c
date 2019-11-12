a93a7166e67cc9d87732d5ca871becc0e1a7f1eb
juliancastaeda <1277@holbertonschool.com>
2019-11-12 15:45:58 -0500

    load commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *ptr;
 ssize_t a;
-int fd = open(filename, O_RDONLY);
 
-if (fd == -1)
-return (0);
 if (filename == NULL)
+return (0);
+int fd = open(filename, O_RDONLY);
+if (fd == -1)
 return (0);
 ptr = malloc(sizeof(letters));
 if (ptr == NULL)
