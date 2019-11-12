cd7485bae20452462f41f83e160099b9e5a184a6
juliancastaeda <1277@holbertonschool.com>
2019-11-12 14:52:01 -0500

    load README, file holberton.h and task number 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - read filename
+ * @filename: string of characters to be printed
+ * @letters: filename size
+ * Return: (ssize_t)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *ptr;
+int a;
+int fd = open(filename, O_RDONLY);
+
+if (fd == '\0')
+{
+return (0);
+}
+ptr = malloc(sizeof(fd * letters));
+if (letters == '\0')
+{
+return (0);
+}
+{
+a = read(fd, ptr, letters);
+
+write(STDIN_FILENO, ptr, letters);
+{
+return (a);
+}
+}
+return (0);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef FILE_I_O
+#define FILE_I_O
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
