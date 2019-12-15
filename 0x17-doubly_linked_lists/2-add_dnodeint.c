145a4c64578942dfa36a9ed1e682ada09fe6a551
juliancastaeda <1277@holbertonschool.com>
2019-12-14 21:40:25 -0500

    upload point 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
-
 /**
- * add_dnodeint: adition new node 
- * @n: new node 
- * @head: init double linked lists 
- * Return: new element or NULL 
- */
+ * add_dnodeint - adition new node
+ * @head: new node
+ * @n: init double linked lists
+ *
+ * Return: new element or NULL
+ **/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new;
