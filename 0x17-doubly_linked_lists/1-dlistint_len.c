f023e0d7719f30097586133f4cff0d67d35c891d
juliancastaeda <1277@holbertonschool.com>
2019-12-14 21:34:37 -0500

    upload point 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,10 +10,10 @@ size_t dlistint_len(const dlistint_t *h)
 {
 size_t a = 0;
 
-for (count = 0; h != NULL; count++)
+for (a = 0; h != NULL; a++)
 {
 h = h->next;
 }
-return (count);
+return (a);
 }
 
