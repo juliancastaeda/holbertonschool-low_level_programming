c54e3ea078b8cbca567c9c04ef3decbd793f8093
juliancastaeda <1277@holbertonschool.com>
2019-12-14 22:25:44 -0500

    load point 4 and lists.h

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * free_dlistint - free a double linked lists
+  * @head: pointer
+  * 
+  * return : (0)
+  **/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *clear = head;
+
+while (clear)
+{
+free(&head->n);
+head = clear->next;
+clear = clear->next;
+}
+free(head);
+}
