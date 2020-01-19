ecd95106ff9317ec9dcbccb4a5ff9e973a6a095f
juliancastaeda <1277@holbertonschool.com>
2020-01-19 14:52:25 -0500

    load point 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hash table
+ * @size: table
+ *
+ * Return: point hash
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *a = NULL;
+
+a = malloc(sizeof(hash_table_t));
+if (a == NULL)
+return (NULL);
+if (size <= 0)
+return (NULL);
+a->size = size;
+a->array = malloc(sizeof(hash_node_t *) * size);
+if (a->array == NULL)
+{
+return (NULL);
+}
+return (a);
+}
